dad3dcc64731c0adc84bab1389f5dcd4fc0ea40e
Berlin Johnson <blackbear@Berlins-MacBook-Pro.local>
2021-02-06 22:29:06 +0900

    Greetings

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Greeting } from './components/Greeting'
+
 
 function App() {
+ 
+
+  let noun = 'Son';
+  let url = "https://reactjs.org"
   return (
     <div className="App">
       <header className="App-header">
+        <Greeting />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          What's Good {noun}!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href={url}
           target="_blank"
           rel="noopener noreferrer"
         >
