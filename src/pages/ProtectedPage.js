85a9586d898eaf74663fc226c62ac9d6320268c7
Berlin Johnson <blackbear@Berlins-MacBook-Pro.local>
2021-02-21 22:27:36 +0900

    use useEffect to redirect

--- src/pages/ProtectedPage.js
+++ src/pages/ProtectedPage.js
@@ -1,13 +1,21 @@
-import React from "react";
-import { Redirect } from "react-router-dom";
+import React, {useEffect} from "react";
+import { useHistory } from "react-router-dom";
 
 // set isAuthed to false to automatically get redirected to home page
 // set isAuthed to true to get to the ProtectedPage
+// use useHistory and useEffect to redirect to home page
 export const ProtectedPage = () => {
-  const isAuthed = false;
-  return isAuthed ? (
+
+  const history = useHistory();
+  const isAuthed = true;
+
+  useEffect(() => {
+    if(!isAuthed){
+      history.push('/');
+    }
+  })
+
+  return (
     <h1>Only authorized users should be able to see this page.</h1>
-  ) : (
-    <Redirect to="/" />
-  );
+  )
 };
