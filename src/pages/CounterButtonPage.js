baa27ebce3f59e051a05d29e78525724fcd77cd7
Berlin Johnson <blackbear@Berlins-MacBook-Pro.local>
2021-02-25 17:22:36 +0900

    Store data locally - Persist data

--- src/pages/CounterButtonPage.js
+++ src/pages/CounterButtonPage.js
@@ -1,27 +1,28 @@
-import React, { useState } from "react";
-import { useLocation } from "react-router-dom";
-import { parse } from "query-string";
+import React, { useState, useEffect } from "react";
 import { CounterButton } from "../components/CounterButton";
 import { CongratulationsMessage } from "../components/CongratulationsMessage";
 import { DisplayIf } from "../DisplayIf";
 
 export const CounterButtonPage = () => {
-  const location = useLocation();
-
-  const startingValue = parse(location.search).startingValue;
   // starting will be a string so wrap in Number for number of clicks
-  const [numberOfClicks, setNumberOfClicks] = useState(Number(startingValue));
+  const [numberOfClicks, setNumberOfClicks] = useState(
+    // get the numberOfClicks value, convert from string to number and set default value to 0
+    Number(localStorage.getItem("numberOfClicks"))
+  );
+  // set number of clicks value in local storage every time numberOfClick state changes
+  useEffect(() => {
+    localStorage.setItem("numberOfClicks", numberOfClicks);
+  }, [numberOfClicks]);
+
   const increment = () => setNumberOfClicks(numberOfClicks + 1);
 
   return (
     <>
       <h1>The Counter Button page</h1>
       <DisplayIf condition={!numberOfClicks >= 10}>
-        <CongratulationsMessage
-          threshold={10}
-        />
+        <CongratulationsMessage threshold={10} />
       </DisplayIf>
       <CounterButton onIncrement={increment} numberOfClicks={numberOfClicks} />
-    </> 
+    </>
   );
 };
